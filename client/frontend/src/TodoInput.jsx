1349afbafcb6ec0b76261eda6b7341f0f877822a
Giriraj Singh Shekhawat <giriraj3103@gmail.com>
2024-01-29 23:56:35 +0530

    ui work

--- client/frontend/src/TodoInput.jsx
+++ client/frontend/src/TodoInput.jsx
@@ -30,28 +30,31 @@ const handleSubmit=(e)=>{
 }
 
     return (
-        <div>
+        <div className='flex flex-col h-[300px] w-[400px] border-2 border-indigo-600  '>
          <form onSubmit={handleSubmit}>
-            <div>
-                 <label htmlFor='tittle'>Tittle</label>
+            <div className='  '>
+                 <label htmlFor='tittle' className='h-[40px] w-full'>Tittle</label>
                 <input type='text' required placeholder='Tittle' name='tittle' id='tittle' 
                 value={data.tittle}
                 onChange={handleChange}
+                className='h-[40px]   w-full mt-[8px] '
                 />
             </div>
-            <div>
-                 <label htmlFor='task'>Task</label>
+            <div className='h-[40px] w-full mt-[15px]  '>
+                 <label htmlFor='task'  className='h-[40px] w-full   ' >Task</label>
                 <textarea type='text' required placeholder='Write your task' name='task' id='task'
-                value={data.task} 
-                onChange={handleChange}/>
+               rows={7} cols={50} 
+               value={data.task} 
+                onChange={handleChange}
+                className='     mt-[8px]   '
+                />
             </div>
-            {/* <div>
-                <label htmlFor='isCompleted'>Completed</label>
-                <input type='radio' name='isCompleted' id='isCompleted' value={data.isCompleted}
-                onChange={handleCompletedButton}/>
-            </div> */}
-            <button type='sumbit'>Make Task</button>
+            <div className='mt-[170px]'>
+            <button type='sumbit' className='  bg-blue-700'>Make Task</button>
+            </div>
+             
          </form>
+          
         </div>
     );
 }
